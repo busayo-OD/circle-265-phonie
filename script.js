b063cc9b06cbef70402c5ba46c553ef078ec3196
Ridwan Adebayo <38481396+temitope3201@users.noreply.github.com>
2022-07-25 18:15:55 +0000

    Restored Initial Setup

--- script.js
+++ script.js
@@ -102,17 +102,3 @@ function country_code() {
 
 
 // JOSEPH AND RIDWAN PLEASE INPUT YOUR CODE HERE
-
-
-function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-   
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
